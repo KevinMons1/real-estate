fd0a414d0d6d5b9766bed773770426625370e825
Kevin Monsieur <62358478+KevinMons1@users.noreply.github.com>
2023-03-07 09:04:02 +0100

    Select integration
    Select integration

--- src/components/inputs/selectCustom.tsx
+++ src/components/inputs/selectCustom.tsx
@@ -1,10 +1,51 @@
-import React from "react";
-import Select from "react-select/base";
+import React, { useState } from "react";
 
-const SelectCustom = () => {
+interface PropsType {
+    selectClassName?: string,
+}
+
+interface IValue {
+    value: string,
+    label: string
+}
+
+interface ArrayObjectSelectState {
+    selectedValue: IValue | null;
+}
+
+const options: IValue[] = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+]
+
+const SelectCustom: React.FC<PropsType> = (
+    {
+        selectClassName = 'select__default',
+    }) => {
+
+    const [selectedOption, setSelectedOption] = useState<ArrayObjectSelectState>({
+        selectedValue: null,
+    });
+
+    const handleChange = (option: IValue | null) => {
+        setSelectedOption({
+            selectedValue: option,
+        });
+    };
+
     return (
-        <>
-        </>
+        <div className={`select__container select--reset ${selectClassName}`}>
+            <select>
+                {options.map((option, index) => {
+                    return (
+                        <option key={`option-${index}`} value={option.value}>
+                            {option.label}
+                        </option>
+                    )
+                })}
+            </select>
+        </div>
     )
 }
 
