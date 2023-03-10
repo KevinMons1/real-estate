820d21ef2ceae4441b620cef3a4be067e759e519
Kevin Monsieur <62358478+KevinMons1@users.noreply.github.com>
2023-03-10 08:24:31 +0100

    Select filter
    Select filter

--- src/components/filters/filterBar.tsx
+++ src/components/filters/filterBar.tsx
@@ -1,11 +1,21 @@
-import React from "react";
-import SelectCustom from "../inputs/selectCustom";
+import React from 'react';
+import SelectCustom from '../inputs/selectCustom';
 
 const FilterBar = () => {
     return (
-        <div className="filter__default bg--white spacer__mb--lg spacer__mt--lg">
-
-            <SelectCustom />
+        <div className='filter__default bg--white spacer__mb--lg spacer__mt--lg'>
+            <SelectCustom
+                selectClassName='select__default spacer__pl--md'
+                selectId='id-exemple' />
+            <SelectCustom
+                selectClassName='select__default spacer__pl--md'
+                selectId='id-exemple' />
+            <SelectCustom
+                selectClassName='select__default spacer__pl--md'
+                selectId='id-exemple' />
+            <SelectCustom
+                selectClassName='select__default spacer__pl--md'
+                selectId='id-exemple' />
         </div>
     )
 }
--- src/components/inputs/selectCustom.tsx
+++ src/components/inputs/selectCustom.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 interface PropsType {
     selectClassName?: string,
+    selectId: string,
 }
 
 interface IValue {
@@ -22,11 +23,17 @@ const options: IValue[] = [
 const SelectCustom: React.FC<PropsType> = (
     {
         selectClassName = 'select__default',
+        selectId,
     }) => {
 
+    const [selected, setSelected] = useState(null);
     const [selectedOption, setSelectedOption] = useState<ArrayObjectSelectState>({
         selectedValue: null,
     });
+
+    const handleOnChangeSelect = (event: React.FormEvent<HTMLSelectElement>) => {
+        const newValue = event.currentTarget.value;
+    };
 
     const handleChange = (option: IValue | null) => {
         setSelectedOption({
@@ -35,8 +42,26 @@ const SelectCustom: React.FC<PropsType> = (
     };
 
     return (
-        <div className={`select__container select--reset ${selectClassName}`}>
-            <select>
+        <div className={`select__container ${selectClassName}`}>
+            <div className="select__wrapper">
+                <div className="select__header">
+                    <span>Looking for</span>
+                </div>
+                <ul className="select__content">
+                    {options.map((option, index) => {
+                        return (
+                            <li key={`option-${index}`} value={option.value}>
+                                {option.label}
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+            <select
+                id={selectId}
+                className='select--hide'
+                onChange={handleOnChangeSelect}
+            >
                 {options.map((option, index) => {
                     return (
                         <option key={`option-${index}`} value={option.value}>
